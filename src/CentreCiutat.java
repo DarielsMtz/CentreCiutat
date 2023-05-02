73f58689429dc953a1fe9684b35d6dc49d84f3b0
Dariels Mtz <darielsmartinez926@gmail.com>
2023-05-02 20:43:13 +0200

    Commit Dariels 2

--- src/CentreCiutat.java
+++ src/CentreCiutat.java
@@ -4,6 +4,8 @@ public class CentreCiutat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola. Mundo!!");
+		
+		System.out.println("Adios, mundo!");
 
 	}
 
