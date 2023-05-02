dfb912f957b9ec88302a44298ea1d6e5d1af39fe
Dariels Mtz <darielsmartinez926@gmail.com>
2023-05-02 20:16:59 +0200

    Primer Commint

--- src/CentreCiutat.java
+++ src/CentreCiutat.java
@@ -3,7 +3,7 @@ public class CentreCiutat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola. Mundo");
+		System.out.println("Hola. Mundo!!");
 
 	}
 
