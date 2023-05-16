521e32829e7c81fd1c9d94c4e282b1965149c78f
Lluís Llodrà Bordoy <lluisllodra@gmail.com>
2023-05-16 20:05:49 +0200

    Lluis - Copia de la clase con el método en la nueva carpeta 'main'

---
+++ src/main/CentreCiutat.java
@@ -0,0 +1,27 @@
+package main;
+import java.util.*;
+
+public class CentreCiutat {
+
+	public static void main(String[] args) {
+
+Scanner in = new Scanner (System.in);
+		
+		System.out.println("=================================");
+		System.out.println("===== Centre Ciutat Parking =====");
+		System.out.println("=================================");
+		
+		System.out.println("\n*** Inicio de Sesión ***");
+		System.out.println("Por favor, introduzca los siguientes datos:");
+		System.out.print("Usuario: ");
+		String usuario = in.next();
+		System.out.print("Contraseña: ");
+		String contraseña = in.next();
+		
+		System.out.println(usuario+" "+contraseña);
+		
+		in.close();
+		
+	}
+
+}
