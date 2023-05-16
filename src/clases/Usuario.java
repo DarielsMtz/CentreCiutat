673be7458282c47ec561e87672cd508a1e8e6aa1
Lluís Llodrà Bordoy <lluisllodra@gmail.com>
2023-05-16 20:03:49 +0200

    Lluis - creacion de la clase 'usuario'

---
+++ src/clases/Usuario.java
@@ -0,0 +1,47 @@
+package clases;
+
+public abstract class Usuario {
+
+	// Atributos generales
+	protected String usuario;
+	protected String contraseña;
+	protected tipoUsuario tipo;
+	public enum tipoUsuario {admin, inquilino}
+	
+	// Constructor
+	public Usuario(String usuario, String contraseña, tipoUsuario tipo) {
+		super();
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+		this.tipo = tipo;
+	}
+
+	// Getters y Setters
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public tipoUsuario getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(tipoUsuario tipo) {
+		this.tipo = tipo;
+	};
+	
+	
+	
+	
+}
