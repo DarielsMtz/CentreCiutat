67755119f756ab2b7e6996a3f486486e11fd9aa4
Julio <julio264edu@gmail.com>
2023-05-25 19:12:18 +0200

    Julio - Conexión a la base

--- src/recursos/BBDD.java
+++ src/recursos/BBDD.java
@@ -1,5 +1,36 @@
 package recursos;
+import java.sql.*;
+
 
 public class BBDD {
 
+
+	public static void main(String[] args) {
+
+		// Creamos la conexion a la base de datos
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ordenadores", "edib", "edib");
+			System.out.println("Conexión establecida correctamente!");
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+
+	}
+
+	// Metodo de SQLException
+	private static void printSQLException(SQLException exception) {
+
+		exception.printStackTrace(System.err);
+		System.err.println("SQLState: " + exception.getSQLState()); // getSQLState()
+		System.err.println("Error Code: " + exception.getErrorCode()); // getErrorCode()
+		System.err.println("Message: " + exception.getMessage()); // getMessage()
+
+		Throwable th = exception.getCause(); // getCause() - Leemos la primera causa
+
+		while (th != null) {
+			System.out.println("Cause: " + th); // Imprimimos una causa
+			th = th.getCause(); // Leemos otra causa
+		}
+
+	}
 }
