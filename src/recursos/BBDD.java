1b9b6234a190d4d04342fc2541a83e2cc09430d0
erick <erick@Erick>
2023-05-26 20:00:10 +0200

    Erick - Corrección,creacion de BBDD, creacion metodo global

--- src/recursos/BBDD.java
+++ src/recursos/BBDD.java
@@ -1,32 +1,31 @@
 package recursos;
+
 import java.sql.*;
 
 public class BBDD {
 	private static final String URL = "jdbc:mysql://localhost:3306/centreciutat";
-    private static final String DB_NAME = "centreciutat";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
-    
-	public static void main(String[] args) {
+	private static final String DB_NAME = "centreciutat";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+
+	protected static void ejectutarMetodos() {
 		try {
+			crearBaseDeDatos();
 			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			System.out.println("Conexión establecida correctamente!");
-			
-			crearTablaUsuarios(con, "centreciutat");
-			crearTablaVehiculo(con, "centreciutat");
-			crearTablaPlazas(con, "centreciutat");
-			crearTablaCliente(con, "centreciutat");
-			agregarPlazas(con, "centreciutat");
-			
+			crearTablaUsuarios(con);
+			crearTablaVehiculo(con);
+			crearTablaPlazas(con);
+			crearTablaCliente(con);
+			agregarPlazas(con);
+
 		} catch (SQLException e) {
 			printSQLException(e);
 		}
-		
-		
 	}
-
+	
 	// Metodo de SQLException
-	private static void printSQLException(SQLException exception) {
+	protected static void printSQLException(SQLException exception) {
 
 		exception.printStackTrace(System.err);
 		System.err.println("SQLState: " + exception.getSQLState()); // getSQLState()
@@ -41,216 +40,209 @@ public class BBDD {
 		}
 
 	}
+
+	public static void crearBaseDeDatos() throws SQLException {
+		// Establece los detalles de conexión a la base de datos
+		String url = "jdbc:mysql://localhost:3306/";
+		String username = "root";
+		String password = "";
+
+		// Crea la conexión
+		Connection con = DriverManager.getConnection(url, username, password);
+
+		// Crea la sentencia para crear la base de datos
+		String createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS " + DB_NAME;
+
+		Statement stmt = null;
+		try {
+			// Crea un Statement
+			stmt = con.createStatement();
+			// Ejecuta la consulta para crear la base de datos
+			stmt.executeUpdate(createDatabaseQuery);
+			System.out.println("La base de datos '" + DB_NAME + "' se ha creado correctamente!");
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			// Cierra el Statement
+			stmt.close();
+		}
+	}
+
 	// Crea la tabla de plazas de estacionamiento
-		public static void crearTablaPlazas(Connection con, String centreciutat) throws SQLException {
+	public static void crearTablaPlazas(Connection con) throws SQLException {
 
-			String createString = "CREATE TABLE IF NOT EXISTS plazas (" + "id_plaza INT PRIMARY KEY AUTO_INCREMENT," 
-									+ "Disponible BOOLEAN NOT NULL," + "tamaño VARCHAR(50) NOT NULL," + "numero_plaza VARCHAR(10) NOT NULL," + " precio DOUBLE NOT NULL" + ")";
-			
-			
-			Statement stmt = null;
-			try {
-				// Creamos un Statement
-				stmt = con.createStatement();
-				// Ejecutamos las consultas
-				stmt.executeUpdate(createString);
-				
-				System.out.println("");
-				System.out.println("Se ha creado la tabla Plaza, correctamente!");
-				System.out.println("Se añadió la foreign key id_cliente_alquiler");
-				
+		String createString = "CREATE TABLE IF NOT EXISTS plazas (" + "id_plaza INT PRIMARY KEY AUTO_INCREMENT,"
+				+ "Disponible BOOLEAN NOT NULL," + "tamaño VARCHAR(50) NOT NULL," + "numero_plaza VARCHAR(10) NOT NULL,"
+				+ " precio DOUBLE NOT NULL" + ")";
 
-			} catch (SQLException e) {
-				printSQLException(e);
-			} finally {
-				stmt.close();
-			}
+		Statement stmt = null;
+		try {
+			// Creamos un Statement
+			stmt = con.createStatement();
+			// Ejecutamos las consultas
+			stmt.executeUpdate(createString);
+
+			System.out.println("");
+			System.out.println("Se ha creado la tabla Plaza, correctamente!");
+			System.out.println("Se añadió la foreign key id_cliente_alquiler");
+
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			stmt.close();
 		}
-		
-		// Metodo para crear la tabla vehiculo
-				private static void crearTablaVehiculo(Connection con, String centreciutat) throws SQLException {
-					String createString = "CREATE TABLE IF NOT EXISTS vehiculo (" + "id_vehiculo INT PRIMARY KEY AUTO_INCREMENT," + 
-											"marca VARCHAR(50) NOT NULL," + "modelo VARCHAR(50) NOT NULL," + "color VARCHAR(50) NOT NULL," + "motor VARCHAR(50) NOT NULL," + "matricula VARCHAR(50) NOT NULL," + "tipo ENUM('coche', 'moto', 'furgoneta') NOT NULL" + ")";
+	}
 
-					Statement stmt = null;
-					try {
-						// Creamos un Statement
-						stmt = con.createStatement();
-						// Ejecuatmos la consulta
-						stmt.executeUpdate(createString);
+	// Metodo para crear la tabla vehiculo
+	protected static void crearTablaVehiculo(Connection con) throws SQLException {
+		String createString = "CREATE TABLE IF NOT EXISTS vehiculo (" + "id_vehiculo INT PRIMARY KEY AUTO_INCREMENT,"
+				+ "marca VARCHAR(50) NOT NULL," + "modelo VARCHAR(50) NOT NULL," + "color VARCHAR(50) NOT NULL,"
+				+ "motor VARCHAR(50) NOT NULL," + "matricula VARCHAR(50) NOT NULL,"
+				+ "tipo ENUM('coche', 'moto', 'furgoneta') NOT NULL" + ")";
 
-						System.out.println("");
-						System.out.println("Se ha creado la tabla Vehiculo correctamente!");
+		Statement stmt = null;
+		try {
+			// Creamos un Statement
+			stmt = con.createStatement();
+			// Ejecuatmos la consulta
+			stmt.executeUpdate(createString);
 
-					} catch (SQLException e) {
-						printSQLException(e);
-					} finally {
-						stmt.close();
-					}
-				}
+			System.out.println("");
+			System.out.println("Se ha creado la tabla Vehiculo correctamente!");
 
-		// Metodo para crear la tabla Clientes
-		private static void crearTablaCliente(Connection con, String centreciutat) throws SQLException {
-			String createString = "CREATE TABLE IF NOT EXISTS clientes ("
-		            + "id_cliente INT PRIMARY KEY AUTO_INCREMENT,"
-		            + "nombre VARCHAR(50) NOT NULL,"
-		            + "apellido VARCHAR(50) NOT NULL,"
-		            + "dni VARCHAR(50) NOT NULL,"
-		            + "direccion VARCHAR(50) NOT NULL,"
-		            + "cuenta_corriente VARCHAR(50) NOT NULL,"
-		            + "id_plaza INT NOT NULL,"
-		            + "id_vehiculo INT NOT NULL,"
-		            + "FOREIGN KEY (id_plaza) REFERENCES plazas(id_plaza),"
-		            + "FOREIGN KEY (id_vehiculo) REFERENCES vehiculo(id_vehiculo)"
-		            + ")";
-			Statement stmt = null;
-
-			try {
-				// Creamos un Statement
-				stmt = con.createStatement();
-				// ejecutamos la consulta
-				stmt.executeUpdate(createString);
-
-				System.out.println("");
-				System.out.println("Se ha creado la tabla Clientes correctamente!!");
-				System.out.println("Se añadió la foreign key id_vehiculo");
-				System.out.println("Se añadió la foreign key id_plaza");
-			} catch (SQLException e) {
-				printSQLException(e);
-			} finally {
-				stmt.close();
-			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			stmt.close();
 		}
+	}
 
-		
+	// Metodo para crear la tabla Clientes
+	protected static void crearTablaCliente(Connection con) throws SQLException {
+		String createString = "CREATE TABLE IF NOT EXISTS clientes (" + "id_cliente INT PRIMARY KEY AUTO_INCREMENT,"
+				+ "nombre VARCHAR(50) NOT NULL," + "apellido VARCHAR(50) NOT NULL," + "dni VARCHAR(50) NOT NULL,"
+				+ "direccion VARCHAR(50) NOT NULL," + "cuenta_corriente VARCHAR(50) NOT NULL,"
+				+ "id_plaza INT NOT NULL," + "id_vehiculo INT NOT NULL,"
+				+ "FOREIGN KEY (id_plaza) REFERENCES plazas(id_plaza),"
+				+ "FOREIGN KEY (id_vehiculo) REFERENCES vehiculo(id_vehiculo)" + ")";
+		Statement stmt = null;
 
-		// Método para obtener todas las plazas de estacionamiento
-		private static void obtenerPlazas(Connection con, String centreciutat) throws SQLException {
+		try {
+			// Creamos un Statement
+			stmt = con.createStatement();
+			// ejecutamos la consulta
+			stmt.executeUpdate(createString);
 
-			String query = "SELECT * FROM " + centreciutat + ".plazas_estacionamiento";
+			System.out.println("");
+			System.out.println("Se ha creado la tabla Clientes correctamente!!");
+			System.out.println("Se añadió la foreign key id_vehiculo");
+			System.out.println("Se añadió la foreign key id_plaza");
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			stmt.close();
+		}
+	}
 
-			Statement stmt = null;
-			ResultSet rs = null;
+	// Metodo para crear Usuario
+	protected static void crearTablaUsuarios(Connection con) throws SQLException {
 
-			try {
-				// Creamos un Statement
-				stmt = con.createStatement();
+		String createString = "CREATE TABLE IF NOT EXISTS usuarios (" + "id_usuario INT PRIMARY KEY AUTO_INCREMENT,"
+				+ "tipo VARCHAR(20) NOT NULL," + "nombre_usuario VARCHAR(50) NOT NULL,"
+				+ "contrasena VARCHAR(50) NOT NULL" + ")";
 
-				// Ejecutamos la consulta
-				rs = stmt.executeQuery(query);
+		Statement stmt = null;
+		try {
+			// Creamos un Statement
+			stmt = con.createStatement();
 
-				// Iteramos sobre los resultados
-				while (rs.next()) {
-					int idPlaza = rs.getInt("id_plaza");
-					boolean estado = rs.getBoolean("estado");	
-					String tamaño = rs.getString("tamaño");
-					int numeroPlaza = rs.getInt("numero_plaza");
-					double precio = rs.getDouble("precio");
+			// ejecutamos la consulta
+			stmt.executeUpdate(createString);
+			System.out.println("");
+			System.out.println("Se ha creado la tabla de Usuarios correctamente!");
 
-					// Imprimimos los datos de cada plaza
-					System.out.println("ID Plaza: " + idPlaza);
-					System.out.println("Estado: " + estado);
-					System.out.println("Tamaño: " + tamaño);
-					System.out.println("Número de plaza: " + numeroPlaza);
-					System.out.println("Precio: " + precio);
-					System.out.println("---------------------------");
-				}
-
-				System.out.println("");
-				System.out.println("Consulta de plazas finalizada!");
-
-			} catch (SQLException e) {
-				printSQLException(e);
-			} finally {
-
-				// Cerramos los recursos
-				if (rs != null) {
-					rs.close();
-				}
-				if (stmt != null) {
-					stmt.close();
-				}
-			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			stmt.close();
 		}
 
-		// Metodo para crear Usuario
-		private static void crearTablaUsuarios(Connection con, String centreciutat) throws SQLException {
+	}
 
-			String createString = "CREATE TABLE IF NOT EXISTS usuarios (" + "id_usuario INT PRIMARY KEY AUTO_INCREMENT,"
-					+ "tipo VARCHAR(20) NOT NULL," + "nombre_usuario VARCHAR(50) NOT NULL,"
-					+ "contrasena VARCHAR(50) NOT NULL" + ")";
+	// Metodo para agregar los usuario
+	protected static void agregarUsuarios(Connection con) throws SQLException {
 
-			Statement stmt = null;
-			try {
-				// Creamos un Statement
-				stmt = con.createStatement();
+		Statement stmt = null;
 
-				// ejecutamos la consulta
-				stmt.executeUpdate(createString);
-				System.out.println("");
-				System.out.println("Se ha creado la tabla de Usuarios correctamente!");
+		try {
+			stmt = con.createStatement();
+			// Campos de la tabla usuarios
+			// ID, Tipo, Nombre_Usuario, Contraseña
+			stmt.executeUpdate("INSERT INTO usuarios (tipo, nombre_usuario, contrasena) VALUES "
+					+ "('admin', 'admin123', 'admin123')," // Usuario para administrador
+					+ "('usuario', 'usuario123', 'usuario123')," // Usuario para un primer usuario normal
+					+ "('usuario', 'usuario456', 'usuario456')"); // Usuario para un segundo usuario normal
 
-			} catch (SQLException e) {
-				printSQLException(e);
-			} finally {
-				stmt.close();
-			}
+			System.out.println("");
+			System.out.println("Se han agregado los usuarios correctamente!");
 
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			stmt.close();
 		}
+	}
 
-		// Metodo para agregar los usuario
-		private static void agregarUsuarios(Connection con, String centreciutat) throws SQLException {
+	protected static void agregarPlazas(Connection con) throws SQLException {
+	    Statement stmt = null;
+	    ResultSet rs = null;
 
-			Statement stmt = null;
+	    try {
+	        stmt = con.createStatement();
 
-			try {
-				stmt = con.createStatement();
-				// Campos de la tabla usuarios
-				// ID, Tipo, Nombre_Usuario, Contraseña
-				stmt.executeUpdate("INSERT INTO usuarios (tipo, nombre_usuario, contrasena) VALUES "
-						+ "('admin', 'admin123', 'admin123')," // Usuario para administrador
-						+ "('usuario', 'usuario123', 'usuario123')," // Usuario para un primer usuario normal
-						+ "('usuario', 'usuario456', 'usuario456')"); // Usuario para un segundo usuario normal
+	        // Consulta para contar los registros en la tabla plazas
+	        String countQuery = "SELECT COUNT(*) AS count FROM " + DB_NAME + ".plazas";
+	        rs = stmt.executeQuery(countQuery);
+	        
+	        if (rs.next()) {
+	            int count = rs.getInt("count");
+	            
+	            if (count == 0) {
+	                // La tabla plazas está vacía, proceder a insertar los registros
 
-				System.out.println("");
-				System.out.println("Se han agregado los usuarios correctamente!");
+	                // Campos de la tabla plazas
+	                // ID, disponible, tamaño, numero_plaza, precio
+	                stmt.executeUpdate("INSERT INTO " + DB_NAME + ".plazas (disponible, tamaño, numero_plaza, precio) VALUES "
+	                        + "(true, '10m2', '1A21', 80)," // 1 plaza
+	                        + "(true, '10m2', '1A22', 80)," // 2 plaza
+	                        + "(true, '10m2', '1A23', 80)," // 3 plaza
+	                        + "(true, '10m2', '1A24', 80)," // 4 plaza
+	                        + "(false, '10m2', '1A25', 80)," // 5 plaza
+	                        + "(true, '10m2', '1A26', 80)," // 6 plaza
+	                        + "(false, '10m2', '1A27', 80)," // 7 plaza
+	                        + "(true, '10m2', '1A28', 80)," // 8 plaza
+	                        + "(true, '10m2', '1A29', 80)," // 9 plaza
+	                        + "(false, '10m2', '1A30', 80)" // 10 plaza
+	                );
 
-			} catch (SQLException e) {
-				printSQLException(e);
-			} finally {
-				stmt.close();
-			}
-		}
-		
-		// Metodo para agregar las plazas
-				private static void agregarPlazas(Connection con, String centreciutat) throws SQLException {
+	                System.out.println("");
+	                System.out.println("Se han agregado las plazas correctamente!");
+	            } else {
+	                System.out.println("La tabla plazas no está vacía. No se agregaron las plazas.");
+	            }
+	        }
 
-					Statement stmt = null;
+	    } catch (SQLException e) {
+	        printSQLException(e);
+	    } finally {
+	        if (rs != null) {
+	            rs.close();
+	        }
+	        if (stmt != null) {
+	            stmt.close();
+	        }
+	    }
+	}
 
-					try {
-						stmt = con.createStatement();
-						// Campos de la tabla plazas
-						// ID, disponible, tamaño, numero_plaza, precio
-						stmt.executeUpdate("INSERT INTO plazas (disponible, tamaño, numero_plaza, precio) VALUES "
-								+ "(true, '10m2', 'A20', 71)," // 1 plaza
-								+ "(true, '20m2', 'A21', 90)," // 2 plaza
-								+ "(true, '30m2', 'A22', 50)," // 3 plaza
-								+ "(true, '40m2', 'A23', 67)," // 4 plaza
-								+ "(true, '50m2', 'A24', 75)," // 5 plaza
-								+ "(false, '60m2', 'A25', 72)," // 6 plaza
-								+ "(true, '70m2', 'A26', 62)," // 7 plaza
-								+ "(false, '80m2', 'A27', 87)," // 8 plaza
-								+ "(true, '90m2', 'A28', 84)," // 9 plaza
-								+ "(true, '100m2', 'A29', 91)," // 10 plaza
-								+ "(false, '110m2', 'A230', 100)" );// 11 plaza
 
-						System.out.println("");
-						System.out.println("Se han agregado las plazas correctamente!");
-
-					} catch (SQLException e) {
-						printSQLException(e);
-					} finally {
-						stmt.close();
-					}
-				}
 }
