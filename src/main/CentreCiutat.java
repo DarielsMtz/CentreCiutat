4f8073595f3ea8b2773a5e1c331cae9c99a86a9a
DarielsMtz <127892871+DarielsMtz@users.noreply.github.com>
2023-06-02 18:59:51 +0200

    Dariels - Redacción de comentarios Javadoc #4

--- src/main/CentreCiutat.java
+++ src/main/CentreCiutat.java
@@ -1,22 +1,34 @@
 package main;
 
 import recursos.MenuInicioSesion;
+
 /**
+ * Clase principal que inicia la aplicación Centre Ciutat. Esta clase contiene
+ * el método principal, el cual se encarga de crear una instancia de la clase
+ * MenuInicioSesion y llamar al método mostrarMenu() para mostrar el menú de
+ * inicio de sesión y permitir al usuario seleccionar una opción.
  * 
- * @author Dariels Mtz
- *
+ * La clase CentreCiutat es el punto de entrada de la aplicación y actúa como un
+ * controlador para gestionar el flujo de ejecución del programa.
+ * 
+ * @param args
  */
 public class CentreCiutat {
 
 	/**
+	 * Método principal que se encarga de iniciar la aplicación. Crea una instancia
+	 * de la clase MenuInicioSesion y llama al método mostrarMenu() para mostrar el
+	 * menú de inicio de sesión y permitir al usuario seleccionar una opción.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
+		// Se intacion un objeto de tipo MunuInicioSesion
 		MenuInicioSesion inicio = new MenuInicioSesion();
+
+		// Se hace un llamado al menu complento
 		inicio.mostrarMenu();
-		
+
 	}
 }
-
